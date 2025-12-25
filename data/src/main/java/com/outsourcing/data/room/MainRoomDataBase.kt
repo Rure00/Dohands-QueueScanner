8e231df42170d480966f0f47dc4a3277cf2ef14d
rure00 <asnjkd@naver.com>
2025-12-25 23:07:11 +0900

    fix: room data base

--- data/src/main/java/com/outsourcing/data/room/MainRoomDataBase.kt
+++ data/src/main/java/com/outsourcing/data/room/MainRoomDataBase.kt
@@ -3,9 +3,10 @@ package com.outsourcing.data.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.outsourcing.data.dao.LocalJobDao
 import com.outsourcing.domain.entities.Job
 
 @Database(entities = [Job::class], version = 1)
-class MainRoomDataBase {
-
+abstract class MainRoomDataBase {
+    abstract val localJobDao: LocalJobDao
 }
